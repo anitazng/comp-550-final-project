7973a95f8a5668730773009a78403142a77982c7
sohpei <sohpeiwu@gmail.com>
2023-11-30 11:11:28 -0500

    added data, stats test

---
+++ sophie/stats.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import statsmodels.api as sm
+
+# Load the data from the CSV file
+df = pd.read_csv('results.csv')
+
+# Define the independent and dependent variables
+X = df['similarity']
+y = df['baseline']
+
+# Add a constant to the independent variable (required for the regression)
+X = sm.add_constant(X)
+
+# Fit the probit model
+model = sm.OLS(y, X)
+result = model.fit()
+
+# Print the summary of the regression
+print(result.summary())
